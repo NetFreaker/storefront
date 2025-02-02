110668e57fe9f86e6b6f58f8fa9fc9bc642c8197
netfreaker <rahulkavati0406@gmail.com>
2025-02-01 19:32:26 -0800

    signup and login

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,3 +1,36 @@
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 from django.db import models
 
-# Create your models here.
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, role=None, **extra_fields):
+        if not email:
+            return ValueError('Email field is mandatory!')
+        email = self.normalize_email(email)
+        user = self.model(email=email, role=role)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+class CustomUser(AbstractBaseUser):
+    ROLE_CHOICES = (
+        ('customer', 'Customer'),
+        ('service_provider', 'Service Provider'),
+        ('admin', 'Admin')
+    )
+
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='customer')
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(auto_now_add=True)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name']
+
+    def __str__(self):
+        return self.email
+
--- apps/users/urls.py
+++ apps/users/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('hello/', views.say_hello, name='hello')
+    path('signup/', views.SignupView.as_view(), name='signup'),
+    path('login/', views.LoginView.as_view(), name='login'),
 ]
